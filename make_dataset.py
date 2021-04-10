a303f8d4f3a9c7a0d88233de38d57d9561cb5f13
jj1guj <jj1guj@jj1guj.net>
2021-04-10 12:03:22 +0900

    ディレクトリを自動で作成するようにした

--- make_dataset.py
+++ make_dataset.py
@@ -1,15 +1,20 @@
 import random
+import os
 
 datasets=50#吐き出すデータセット数
 min_val=-10**29#乱数の最小値
 max_val=10**29#乱数の最大値
 
-path="in\\"#吐き出し先のパス
+path="in"#吐き出し先のパス
+
+#ディレクトリが存在しなかったら作成
+if not os.path.isdir(path):
+    os.makedirs(path)
 
 for d in range(datasets):
     N=random.randint(2,2048)
     A=["{:30f}".format(random.uniform(min_val,max_val))[:30] for i in range(N)]
     A=[i if i[-1]!="." else i.replace(".","") for i in A]
-    filename=path+str(d).zfill(4)+".in"
+    filename=os.path.join(path,str(d).zfill(4)+".in")
     with open(filename,mode='w') as f:
         f.write("\n".join(A))
